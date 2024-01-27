05a31d694ccf31e0d49af89f65784015a290f034
30shrey <shreya.sschow@gmail.com>
2024-01-27 08:03:16 +0000

    create and implement test  route

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const express = require("express");
 
 const connectDB = require("./config/db");
 const cors= require("cors");
+const furnitures = require("./routes/api/furnitures");
 //Connecting MONGODB
-connectDB();
+//connectDB();
 
 
 
@@ -12,8 +13,11 @@ const app = express();
 
 //Enabling CORS here for safety of access of info from other domain
 app.use(cors());
+
 //Handle JSON parsing properly
-app.use(express.json)
+app.use(express.json);
+//Use the api group instead of multiple paths for multiple routes
+app.use("/api/furnitures", furnitures);
 
 const port = process.env.PORT || 5000
 app.listen(port,() => 
