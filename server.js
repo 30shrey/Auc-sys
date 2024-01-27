c76c970f09ce4d4f78a810e8ab9f0fe0dc442043
30shrey <shreya.sschow@gmail.com>
2024-01-27 07:29:21 +0000

    initiated database

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
-const express = require("express")
+const express = require("express");
+
+const connectDB = require("./config/db");
+const cors= require("cors");
+//Connecting MONGODB
+connectDB();
+
+
+
 //Initiating the app
-const app = express()
+const app = express();
+
+//Enabling CORS here for safety of access of info from other domain
+app.use(cors());
+//Handle JSON parsing properly
+app.use(express.json)
+
 const port = process.env.PORT || 5000
 app.listen(port,() => 
     console.log(`Server is running on port ${port}`)
-)
+);
