ce62351db835076fcb62b5f3c9d283fa20b235bf
30shrey <shreya.sschow@gmail.com>
2024-01-30 09:43:23 +0000

    add all routes

--- routes/api/furnitures.js
+++ routes/api/furnitures.js
@@ -9,6 +9,7 @@ router.get("/test", (req,res) =>{
 //@route GET api/furnitures
 //@description  Give all the furnitures
 //@ access public
+//READ  operation 
 
 router.get('/',(req, res) => {
    // Furniture.find()
@@ -17,8 +18,65 @@ router.get('/',(req, res) => {
     // .json({nofurnituresfound: 'Furniture  Not Found'})
     res.json({OK:"All furniture route is working fine"})
 });
+//@route GET api/furnitures/id
+//@description Get sinlgle furniture details searching  by the id
+//@access public
+//read and search operation 
+ router.get("/:id", (req ,res) => {
+    // Furniture.findById(req.params.id)
+    // .then((furniture) => res.json(furniture))
+    // .catch((err) => res.status(404)).json({nofurniturefound: No Furniture Found})
+    res.json({
+        Ok:"Get Furnitures By Id route is working fine"
+    })
+ })
 
+//@route GET api/furnitures
+//@description  added  furnitures
+//@ access public
+//Create an Furniture  
 
+router.post("/",(req, res) => {
+//      Furniture.create(req.body)
+//      .then((furnitures) => res.json({
+//        msg: "Furniture addded successfully"
+//}))
+//       .catch((err) => res.status(400)).json({
+//        error: "Unable to add the furniture"
+//     })
+// });
+     res.json({
+        OK:"All furniture route is working fine"
+    })
+ });
+
+ //@route GET api/furnitures
+//@description  added  furnitures
+//@ access public
+//READ,UPDATE,SEARCH OPERATION  
+ router.put("/:id",(req, res) => {
+    // Furniture.findByIdAndUpdate(req.params.id, req.body)
+     //.then((furniture) => res.json({msg: "Updatedsuccessfully"})
+     // .catch((err) =>
+     // res.status(400))
+     // .json({error: "unable to find the furniture"})
+     res.json({OK:"Update furniture working is fine"})
+ });
+  //@route GET api/furnitures
+//@description  Find a furniture by id and delete it
+//@ access public
+//DELETE,SEARCH OPERATION  
+router.put("/:id",(req, res) => {
+    // Furniture.findByIdAndRemove(req.params.id, req.body)
+     //.then((furniture) => res.json({
+        //msg: "RemovedSuccessfully"})
+     // .catch((err) =>
+     // res.status(404))
+     // .json({error: "Couldn't  delete the furniture"})
+     res.json({
+        OK:"Delete furniture working is fine"
+    })
+ });
 
 
 module.exports = router;
